7a493161f4acf2c92c7144da46de769d61618ee9
Kacper Karczewski <kacpikarczewski333@gmail.com>
2025-10-01 10:03:40 +0200

    changes in urls.py

--- MySite/blog/urls.py
+++ MySite/blog/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 app_name = 'blog'
 urlpatterns = [
 # widoki posta
     path('', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
