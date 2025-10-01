ae9007a537a400c8d56744cf598701e967f653ad
Kacper Karczewski <kacpikarczewski333@gmail.com>
2025-10-01 13:06:51 +0200

    I domnt knom....
    nuthin wurks, girl help :c

--- MySite/MySite/urls.py
+++ MySite/MySite/urls.py
@@ -1,10 +1,8 @@
+from django.contrib import admin
 from django.urls import path, include
-from . import views
-app_name = 'blog'
+
 urlpatterns = [
-# widoki posta
-    path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
-    path('blog/', include('blog.urls', namespace='blog')),
-    
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', 
+    namespace='blog')),
 ]
--- MySite/blog/urls.py
+++ MySite/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from . import views
 app_name = 'blog'
+
 urlpatterns = [
 # widoki posta
     path('', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'),
     path('blog/', include('blog.urls', namespace='blog')),
+
 ]
